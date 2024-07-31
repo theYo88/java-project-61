f48f789fa1cca3f0cd990b4b9da1647d4319e9fd
VitaliyTsar <theyo88@gmail.com>
2024-07-31 13:11:20 +0300

    add Makefile with run-dist command, create Cli.java with greeting and refactoring App.java, add new section tasks to build.gradle

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,17 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void greetUser() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name? ");
+
+        String name = scanner.nextLine();
+
+        System.out.println("Hello, " + name + "!");
+        scanner.close();
+    }
+}
